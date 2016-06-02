8d3c4f11baae3dc3fabd37604e571e34c239184b
Venkat Peri <venkatperi@gmail.com>
2016-06-02 17:06:50 -0400

    exec

--- bin/hog.js
+++ bin/hog.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 'use strict';
-require('coffee-script/register');
+require( 'coffee-script/register' );
 process.title = 'hog';
-var findup = require( 'findup-sync' );
-var resolve = require( 'resolve' ).sync;
-var basedir = process.cwd();
-var hogpath;
-
-try {
-  hogpath = resolve( 'hog', {basedir : basedir} );
-} catch ( ex ) {
-  hogpath = findup( 'lib/hog.coffee');
-
-  // No hog install found!
-  if ( !hogpath ) {
-    console.log( 'Unable to find local hog.' );
-    process.exit( 99 )
-  }
-}
+// var findup = require( 'findup-sync' );
+// var resolve = require( 'resolve' ).sync;
+// var basedir = process.cwd();
+// var hogpath;
+//
+// try {
+//   hogpath = resolve( 'hog', {basedir : basedir} );
+// } catch ( ex ) {
+//   hogpath = findup( 'lib/hog.coffee');
+//
+//   No hog install found!
+// if ( !hogpath ) {
+//   console.log( 'Unable to find local hog.' );
+//   process.exit( 99 )
+// }
+// }
 
 //noinspection JSUnresolvedFunction
-var Hog = require( hogpath );
+var Hog = require( '../' );
 new Hog().cli();
