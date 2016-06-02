5ee35fde0e814a345b32f772938f9576d1754b7d
Venkat Peri <venkatperi@gmail.com>
2016-06-02 17:26:36 -0400

    refactored hog out to lean-hog

--- bin/hog.js
+++ bin/hog.js
@@ -2,23 +2,23 @@
 'use strict';
 require( 'coffee-script/register' );
 process.title = 'hog';
-// var findup = require( 'findup-sync' );
-// var resolve = require( 'resolve' ).sync;
-// var basedir = process.cwd();
-// var hogpath;
-//
-// try {
-//   hogpath = resolve( 'hog', {basedir : basedir} );
-// } catch ( ex ) {
-//   hogpath = findup( 'lib/hog.coffee');
-//
-//   No hog install found!
-// if ( !hogpath ) {
-//   console.log( 'Unable to find local hog.' );
-//   process.exit( 99 )
-// }
-// }
+var findup = require( 'findup-sync' );
+var resolve = require( 'resolve' ).sync;
+var basedir = process.cwd();
+var hogpath;
+
+try {
+  hogpath = resolve( 'lean-hog', {basedir : basedir} );
+} catch ( ex ) {
+  hogpath = findup( 'lib/hog.coffee' );
+
+  // No hog install found!
+  if ( !hogpath ) {
+    console.log( 'Unable to find local hog. To install: npm i --save lean-hog' );
+    process.exit( 99 )
+  }
+}
 
 //noinspection JSUnresolvedFunction
-var Hog = require( '../' );
+var Hog = require( hogpath );
 new Hog().cli();
